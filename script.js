563e554f82868c88a26da001085e2adc44c8f8e0
Vadim <vadim.popchenya@mail.ru>
2024-04-05 14:29:10 +0300

    made edits

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 //Burger.js
 
-const infoPanelMobile = document.getElementById('infoPanelMobile');
-const buttonBurger = document.getElementById('buttonBurger');
-const body = document.getElementById('body')
+const infoPanelMobile = document.getElementById("infoPanelMobile");
+const buttonBurger = document.getElementById("buttonBurger");
+const body = document.getElementById("body");
 
-buttonBurger.addEventListener('click', function () {
-  infoPanelMobile.classList.toggle('burger-active');
-  body.classList.toggle('no-scroll');
-})
-
+buttonBurger.addEventListener("click", function () {
+  infoPanelMobile.classList.toggle("burger-active");
+  body.classList.toggle("no-scroll");
+});
 
 //Tooltip.js
-const tooltipList = Array.from(document.querySelectorAll('.tooltip'));
-tooltipList.forEach(tooltip => {
-  console.log(tooltip);
-  tooltip.addEventListener('click', () => {
-tooltip.classList.toggle('tooltip-active')
-  })
-})
-
-;
-
-
+const tooltipList = Array.from(document.querySelectorAll(".tooltip"));
+tooltipList.forEach((tooltip) => {
+  tooltip.addEventListener("click", () => {
+    tooltip.classList.toggle("tooltip-active");
+  });
+});
